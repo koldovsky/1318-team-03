11af2a66fc6a7ecfae9da8245b67317773da4b9e
Sofia Levko <113248354+sofa4578@users.noreply.github.com>
2025-01-09 14:27:56 +0200

    Create our-services.js

---
+++ js/our-services.js
@@ -0,0 +1,54 @@
+        document.addEventListener("DOMContentLoaded", function () {
+            const servicesOptions = document.querySelector(".services__options");
+            const leftButton = document.querySelector(".services__buttons[aria-label='Scroll left']");
+            const rightButton = document.querySelector(".services__buttons[aria-label='Scroll right']");
+            const options = document.querySelectorAll(".services__option");
+
+            let currentIndex = 0;
+            let visibleOptionsCount = 3;
+
+            const updateVisibleOptionsCount = () => {
+
+                if (window.innerWidth <= 480) {
+                    visibleOptionsCount = 1;
+                } else if (window.innerWidth <= 768) {
+                    visibleOptionsCount = 2;
+                } else {
+                    visibleOptionsCount = 3;
+                }
+            };
+
+            const updateCarousel = () => {
+                updateVisibleOptionsCount();
+                options.forEach((option, index) => {
+                    option.style.display = "none";
+
+                    if (index >= currentIndex && index < currentIndex + visibleOptionsCount) {
+                        option.style.display = "block";
+                    }
+                });
+
+                leftButton.disabled = currentIndex === 0;
+                rightButton.disabled = currentIndex >= options.length - visibleOptionsCount;
+            };
+
+            leftButton.addEventListener("click", () => {
+                if (currentIndex > 0) {
+                    currentIndex--;
+                    updateCarousel();
+                }
+            });
+
+
+            rightButton.addEventListener("click", () => {
+                if (currentIndex < options.length - visibleOptionsCount) {
+                    currentIndex++;
+                    updateCarousel();
+                }
+            });
+
+
+            updateCarousel();
+
+            window.addEventListener("resize", updateCarousel);
+        });
